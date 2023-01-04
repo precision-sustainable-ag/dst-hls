6309792d24327fec8b465ffd885345bf0e945921
milad <ubuntu@ip-172-31-49-218.us-west-2.compute.internal>
2023-01-04 08:17:53 +0000

    added docstring

--- dst_hls/scripts/interface.py
+++ dst_hls/scripts/interface.py
@@ -12,6 +12,20 @@ class Interface:
         self.cat = None
 
     def ndvi_images(self, geometry, date_range="2021-05-01/2021-08-02"):
+        """
+        fetches an ndvi image provided for a certain date and geometry
+
+        Arguments:
+            geometry {json} -- heometry to clip image with
+
+        Keyword Arguments:
+            date_range {str} -- datetime range with format:
+            (default: {"2021-05-01/2021-08-02"})
+
+        Returns:
+            numpy array -- an array containing all bands of
+            available images in a flatten shape
+        """
         self.cat = Catalog()
         self.colls = self.cat.search_tiles(geometry,
                                            date_range=date_range
